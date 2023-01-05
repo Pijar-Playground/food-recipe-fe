b272f7a3f2c74e14da1ed59d1a237cf53371de22
Rachmad22 <rachmadnur22@gmail.com>
2023-01-05 14:36:37 +0700

    [ADD] profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Detail from "./pages/Detail";
+import Profile from "./pages/Profile";
 
 // functional component
 function App() {
@@ -22,6 +23,10 @@ function App() {
     {
       path: "detail",
       element: <Detail />,
+    },
+    {
+      path: "profile",
+      element: <Profile />,
     },
   ]);
 
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile() {
+    return (
+        <div>
+            <h1>Ini halaman profile pages</h1>
+        </div>
+    );
+}
+
+export default Profile;
